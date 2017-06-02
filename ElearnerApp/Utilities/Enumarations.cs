b8ec7f668462dc730f4eec552218bc08198baaae
jst11 <eliastamat@gmail.com>
2017-06-02 15:21:48 +0300

    Clear Branch

--- ElearnerApp/Utilities/Enumarations.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace ElearnerApp.Utilities
-{
-    public enum UserType { Guest, LoggedInUser, Admin}
-}
